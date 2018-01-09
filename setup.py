e6cf256edd0b0efe4cd34af61430fae46563cf2d
alvivar <andresalvivar@gmail.com>
2018-01-09 14:47:26 -0600

    cx_Freeze setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+""" Create an .exe with cx_Freeze, by calling 'python setup.py build'. """
+
+from cx_Freeze import Executable, setup
+
+OPTIONS = {
+    'build_exe': {
+        'includes': ['Queue.multiprocessing', 'idna.idnadata']
+    }
+}
+
+EXECUTABLES = [Executable('wikilearnbot.py', targetName='wikilearnbot.exe')]
+
+setup(
+    name='wikilearnbot',
+    version='0.1',
+    description="Bot that tweets random articles with images from Wikipedia",
+    executables=EXECUTABLES,
+    options=OPTIONS)
