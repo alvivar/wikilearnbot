900a2b2dc3d63a6f6ac36fff6248089ea7607e45
alvivar <andresalvivar@gmail.com>
2018-01-17 19:42:00 -0600

    Get articles with images, cool

--- wikiapi.py
+++ wikiapi.py
@@ -1,10 +1,16 @@
 """
     Wikipedia API wrapper for random articles and images.
+
+    @matnesis
+    2018/01/17
 """
 
 import json
 import os
 import sys
+import time
+import urllib
+
 import requests
 
 HOME = os.path.normpath(  # The script directory + cxfreeze compatibility
@@ -12,10 +18,10 @@ HOME = os.path.normpath(  # The script directory + cxfreeze compatibility
         sys.executable if getattr(sys, 'frozen', False) else __file__))
 
 
-def get_random_articles(count=1, lang="en"):
+def get_random_articles(count=1, lang='en'):
     """
-        Return a json with random articles from Wikipedia API. It includes id,
-        title, summary and main image when available.
+        Return a json with random articles from Wikipedia API. It includes
+        'pageid', 'title', 'extract' and 'pageimage' when available.
     """
 
     response = requests.get(
@@ -62,8 +68,46 @@ def get_image_info(imagename):
     return response.json()
 
 
-def get_random_articles_with_images():
-    pass
+def get_image_url(imagename):
+    """
+        Return the url from the Wikipedia image.
+    """
+    info = get_image_info(imagename)
+    return info['query']['pages'][0]['imageinfo'][0]['url']  # Optimism +1
+
+
+def get_random_articles_with_images(count=1, lang='en', rest=5, ignoreids=[]):
+    """
+        Return a json file with a list of random articles that always contain a
+        main image.
+
+        Even when this function uses Wikipedia API below, the json returned is
+        cleaner and compact that a normal Wikipedia response.
+    """
+
+    # Filtering only with images
+
+    withimages = []
+    while len(withimages) < count:
+        ranart = get_random_articles(count=count, lang=lang)
+        withimages += [d for d in ranart['query']['pages'] if 'pageimage' in d]
+        time.sleep(rest)  # Decent +1
+
+    # A simpler json response, including images urls
+
+    result = []
+    for w in withimages:
+        title = urllib.parse.quote_plus(w['title'].replace(' ', '_'))
+        result.append({
+            'pageid': w['pageid'],
+            'pageurl': f"https://{lang}.wikipedia.org/wiki/{title}",
+            'title': w['title'],
+            'extract': w['extract'],
+            'pageimage': w['pageimage'],
+            'pageimageurl': get_image_url(w['pageimage'])
+        })
+
+    return result[:count]
 
 
 if __name__ == "__main__":
@@ -71,7 +115,11 @@ if __name__ == "__main__":
     # Tests
 
     with open(os.path.join(HOME, "sample-random-articles.json"), 'w') as f:
-        json.dump(get_random_articles(count=10), f)
+        json.dump(get_random_articles(count=5), f)
 
     with open(os.path.join(HOME, "sample-image-info.json"), 'w') as f:
         json.dump(get_image_info("USA_Wisconsin_location_map.svg"), f)
+
+    with open(os.path.join(HOME, "sample-random-articles-images.json"),
+              'w') as f:
+        json.dump(get_random_articles_with_images(5), f)
